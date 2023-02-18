1f87cb1e9783742303b15b00938a8f102e00335e
woshityj <61250121+woshityj@users.noreply.github.com>
2023-02-19 03:31:00 +0800

    Adjusted

--- Military_Time_Conversion.py
+++ Military_Time_Conversion.py
@@ -2,7 +2,7 @@ from datetime import datetime
 num2words = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', \
             6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', \
             11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', \
-            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty-One', 22: 'twenty-two', 23: 'twenty-three', 30: 'thirty', 40: 'forty', \
+            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two', 23: 'twenty three', 30: 'thirty', 40: 'forty', \
             50: 'fifty', 0: 'zero', 00: 'zero'}
 
 def convert24(time):
@@ -26,11 +26,21 @@ def convertText(time):
     if minutes_list[0] == "0" and minutes_list[1] == "0":
           string.append("hundred hours")
     else:
-        if int(minutes_list[0]) > 1:
-              minutes_list[0] = minutes_list[0] + "0"
-        for i in range(len(minutes_list)):
-              string.append(num2words[int(minutes_list[i])])
+        if int(minutes_list[0]) >= 1:
+             minutes_list[0] = minutes_list[0] + "0"
+        if int(minutes_list[0]) >= 10 and int(minutes_list[0]) <= 19:
+             minutes = 0
+             for i in range(len(minutes_list)):
+                  minutes = minutes + int(minutes_list[i])
+             string.append(num2words[minutes])
+        else:
+          for i in range(len(minutes_list)):
+                if int(minutes_list[1] != "0"):
+                    string.append(num2words[int(minutes_list[i])])
+                else:
+                    string.append(num2words[int(minutes_list[0])])
+                    break
     return (' '.join(string))
 
-time = convert24("4:00PM")
+time = convert24("7:45AM")
 print(convertText(time))
